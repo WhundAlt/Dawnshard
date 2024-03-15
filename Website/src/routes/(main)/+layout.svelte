b725555d7d99259e226f13cd21c83ad78a9b8ac3
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2024-03-15 01:06:40 +0000

    prettier

--- Website/src/routes/(main)/+layout.svelte
+++ Website/src/routes/(main)/+layout.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import '../../app.pcss';
 	import Header from './header.svelte';
-	import Navigation from './navigation.svelte'
+	import Navigation from './navigation.svelte';
 	import { routeGroups } from './routes';
 </script>
 
 <Header />
-<Navigation routeGroups={routeGroups} />
+<Navigation {routeGroups} />
 
 <main style:padding-top="var(--header-height)" style:padding-left="var(--navigation-width)">
-<slot />
+	<slot />
 </main>
--- Website/src/routes/(main)/header.svelte
+++ Website/src/routes/(main)/header.svelte
@@ -2,24 +2,23 @@
 	import { Button } from '$shadcn/components/ui/button';
 </script>
 
-<style>
-    .header {
-        display: flex;
-        flex-grow: 1;
-        justify-content: space-between;
-				align-items: center;
-        padding: 0 2rem 0 0.75rem;
-				border-bottom: 1px solid;
-				border-color: var(--divider);
-				position: fixed;
-				width: 100vw;
-        height: var(--header-height);
-    }
-</style>
-
-<header class="header">
+<header id="header">
 	<h1 class="scroll-m-20 text-3xl font-bold tracking-tight">Dawnshard</h1>
-	<div style:flex-grow="1"/>
+	<div style:flex-grow="1" />
 	<Button>Login</Button>
 </header>
 
+<style>
+	#header {
+		display: flex;
+		flex-grow: 1;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 2rem 0 0.75rem;
+		border-bottom: 1px solid;
+		border-color: var(--divider);
+		position: fixed;
+		width: 100vw;
+		height: var(--header-height);
+	}
+</style>
